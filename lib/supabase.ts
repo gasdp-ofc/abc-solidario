33d91f1e36489fdb7504031b8682d87b1a2344ef
Guilherme Alves <SEUEMAIL@gmail.com>
2026-05-09 14:29:59 -0300

    Forca URL correta do Supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
-;import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL?.trim() || "";
+const supabaseUrl = "https://psitfprlywjoqcqyelmt.supabase.co";
+
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.trim() || "";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
