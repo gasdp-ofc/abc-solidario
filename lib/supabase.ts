5759ead9b8b5122257aea2b0f02b75b6f6b65e2d
Guilherme Alves <SEUEMAIL@gmail.com>
2026-05-09 14:41:45 -0300

    Corrige conexão Supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+;import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://psitfprlywjoqcqyelmt.supabase.co";
-
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.trim() || "";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
